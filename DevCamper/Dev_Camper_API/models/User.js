b67d7ab3c45f49818738c8b95c7294d0f9b8718f
Michael Coxe <mcoxe@me.com>
2021-03-05 11:38:02 -0900

    finished up authentication and added in user crud routes for admin

--- DevCamper/Dev_Camper_API/models/User.js
+++ DevCamper/Dev_Camper_API/models/User.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -28,7 +29,7 @@ const UserSchema = new mongoose.Schema({
 		select: false,
 	},
 	resetPasswordToken: String,
-	resetPasswordDate: Date,
+	resetPasswordExpire: Date,
 	createdAt: {
 		type: Date,
 		default: Date.now,
@@ -37,6 +38,10 @@ const UserSchema = new mongoose.Schema({
 
 //encrypt password using bcrypt
 UserSchema.pre('save', async function (next) {
+	if (!this.isModified('password')) {
+		next();
+	}
+
 	const salt = await bcrypt.genSalt(10);
 	this.password = await bcrypt.hash(this.password, salt);
 });
@@ -53,4 +58,20 @@ UserSchema.methods.matchPassword = async function (enteredPassword) {
 	return await bcrypt.compare(enteredPassword, this.password);
 };
 
+//Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+	//Generate token
+	const resetToken = crypto.randomBytes(20).toString('hex');
+
+	//hash token and set to reset passworktoken field
+	this.resetPasswordToken = crypto
+		.createHash('sha256')
+		.update(resetToken)
+		.digest('hex');
+
+	//Set expires
+	this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+	return resetToken;
+};
 module.exports = mongoose.model('User', UserSchema);
