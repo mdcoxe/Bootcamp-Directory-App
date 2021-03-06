e960891346ca7b8cc06064a67050586bbeffb2b1
Michael Coxe <mcoxe@me.com>
2021-03-05 17:50:28 -0900

    all functionality complete.  Working on security next

--- DevCamper/Dev_Camper_API/middleware/auth.js
+++ DevCamper/Dev_Camper_API/middleware/auth.js
@@ -10,8 +10,11 @@ exports.protect = asyncHandler(async (req, res, next) => {
 		req.headers.authorization &&
 		req.headers.authorization.startsWith('Bearer')
 	) {
+		//set token from bearer token in header
 		token = req.headers.authorization.split(' ')[1];
 	}
+	//Set token from cookie
+	//Comment out if you don't want to use cookies
 	// else if (req.cookies.token) {
 	// 	token = req.cookies.token;
 	// }
--- DevCamper/Dev_Camper_API/seeder.js
+++ DevCamper/Dev_Camper_API/seeder.js
@@ -10,6 +10,7 @@ dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI, {
@@ -29,12 +30,16 @@ const courses = JSON.parse(
 const users = JSON.parse(
 	fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
+const reviews = JSON.parse(
+	fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+);
 //Import into DB
 const importData = async () => {
 	try {
 		await Bootcamp.create(bootcamps);
 		await Course.create(courses);
 		await User.create(users);
+		await Review.create(reviews);
 		console.log('Data imported...'.green.inverse);
 		process.exit();
 	} catch (err) {
@@ -48,6 +53,7 @@ const deleteData = async () => {
 		await Bootcamp.deleteMany();
 		await Course.deleteMany();
 		await User.deleteMany();
+		await Review.deleteMany();
 		console.log('Data destroyed...'.red.inverse);
 		process.exit();
 	} catch (err) {
